07a325e3b50128f3001ff7cd0cbfb5bdcf56ab48
Hamish <hamisharro@icloud.com>
2021-08-04 10:21:39 +0100

    add test for checking score lower than 50

--- spec/reports_spec.rb
+++ spec/reports_spec.rb
@@ -8,4 +8,8 @@ describe "Reports" do
   it "returns amber when a value of 50 - 74 is entered" do
     expect(report("60")).to eq("Amber: 1")
   end
+
+  it "returns red when a value of 0 - 49 is entered" do
+    expect(report("40")).to eq("Red: 1")
+  end
 end
