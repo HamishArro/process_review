a1e0b23fe5cda665be0a59ab24f9860362c9aec4
Hamish <hamisharro@icloud.com>
2021-08-04 10:23:22 +0100

    change numbers to number

--- lib/reports.rb
+++ lib/reports.rb
@@ -1,6 +1,6 @@
 def report(scores)
-  numbers = scores.to_i
-  if numbers >= 75 then return "Green: 1"
-  elsif numbers >= 50 then return "Amber: 1"
+  number = scores.to_i
+  if number >= 75 then return "Green: 1"
+  elsif number >= 50 then return "Amber: 1"
   else return "Red: 1" end
 end
