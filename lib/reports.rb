a84435897bab436ac3a217c4a1d6ebbb306fd967
Hamish <hamisharro@icloud.com>
2021-08-04 10:20:27 +0100

    made test that checks if score is amber pass

--- lib/reports.rb
+++ lib/reports.rb
@@ -1,4 +1,5 @@
 def report(scores)
   numbers = scores.to_i
-  if numbers >= 75 then return "Green: 1" end
+  if numbers >= 75 then return "Green: 1"
+  elsif numbers >= 50 then return "Amber: 1" end
 end
