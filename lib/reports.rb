949b78052562d0d6916f6dce809cfeb3f14ccb4b
Hamish <hamisharro@icloud.com>
2021-08-04 10:22:48 +0100

    make red test pass

--- lib/reports.rb
+++ lib/reports.rb
@@ -1,5 +1,6 @@
 def report(scores)
   numbers = scores.to_i
   if numbers >= 75 then return "Green: 1"
-  elsif numbers >= 50 then return "Amber: 1" end
+  elsif numbers >= 50 then return "Amber: 1"
+  else return "Red: 1" end
 end
