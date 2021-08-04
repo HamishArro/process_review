ff826afa0a8ea02d087dfac6429c45a3e68ae6ef
Hamish <hamisharro@icloud.com>
2021-08-04 10:35:43 +0100

    make failing test pass

--- lib/reports.rb
+++ lib/reports.rb
@@ -5,8 +5,9 @@ def report(scores)
   array_of_numbers.each { |number|
     if number.to_i >= 75 then green += 1
     elsif number.to_i >= 50 then amber += 1
-    else return "Red: 1" end
+    else red += 1 end
   }
   if green > 0 then result += "Green: #{green}"
-  elsif amber > 0 then result += "Amber: #{amber}" end
+  elsif amber > 0 then result += "Amber: #{amber}"
+  elsif red > 0 then result += "Red: #{red}" end
 end
