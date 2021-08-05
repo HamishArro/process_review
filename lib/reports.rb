cf0aaefa89bc1f6e6bab5784e2f733ae7f8b4020
Hamish <hamisharro@icloud.com>
2021-08-05 20:27:31 +0100

    run rubocop

--- lib/reports.rb
+++ lib/reports.rb
@@ -1,14 +1,14 @@
 def report(scores)
   green = amber = red = 0
-  result = ""
+  result = ''
   array_of_numbers = scores.split(',')
-  array_of_numbers.each { |number|
+  array_of_numbers.each do |number|
     if number.to_i >= 75 then green += 1
     elsif number.to_i >= 50 then amber += 1
     else red += 1 end
-  }
-  if green > 0 then result += "Green: #{green}" end
+  end
+  result += "Green: #{green}" if green > 0
   if amber > 0 then result += result.length > 0 ? "\nAmber: #{amber}" : "Amber: #{amber}" end
   if red > 0 then result += result.length > 0 ? "\nRed: #{red}" : "Red: #{red}" end
-  return result.length > 0 ? result : "No results given"
+  result.length > 0 ? result : 'No results given'
 end
--- spec/reports_spec.rb
+++ spec/reports_spec.rb
@@ -1,40 +1,39 @@
-require "reports"
+require 'reports'
 
-describe "Reports" do
-
-  describe "single input/ type" do
-    it "returns no results given when an empty string is inputed" do
-      expect(report("")).to eq("No results given")
+describe 'Reports' do
+  describe 'single input/ type' do
+    it 'returns no results given when an empty string is inputed' do
+      expect(report('')).to eq('No results given')
     end
 
-    it "returns green when a value of 75+ is entered" do
-      expect(report("80")).to eq("Green: 1")
+    it 'returns green when a value of 75+ is entered' do
+      expect(report('80')).to eq('Green: 1')
     end
 
-    it "returns green: 2 when two values of 75+ is entered" do
-      expect(report("80,90")).to eq("Green: 2")
+    it 'returns green: 2 when two values of 75+ is entered' do
+      expect(report('80,90')).to eq('Green: 2')
     end
 
-    it "returns amber when a value of 50 - 74 is entered" do
-      expect(report("60")).to eq("Amber: 1")
+    it 'returns amber when a value of 50 - 74 is entered' do
+      expect(report('60')).to eq('Amber: 1')
     end
 
-    it "returns amber: 2 when two values of 50 - 74 are entered" do
-      expect(report("50,70")).to eq("Amber: 2")
+    it 'returns amber: 2 when two values of 50 - 74 are entered' do
+      expect(report('50,70')).to eq('Amber: 2')
     end
 
-    it "returns red when a value of 0 - 49 is entered" do
-      expect(report("40")).to eq("Red: 1")
+    it 'returns red when a value of 0 - 49 is entered' do
+      expect(report('40')).to eq('Red: 1')
     end
 
-    it "returns red: 2 when two values of 0 - 49 are entered" do
-      expect(report("30,40")).to eq("Red: 2")
+    it 'returns red: 2 when two values of 0 - 49 are entered' do
+      expect(report('30,40')).to eq('Red: 2')
     end
   end
 
-  describe "different input types" do
+  describe 'different input types' do
     it "returns 'Green: 2\nAmber: 1\nRed: 3' when two values '1,10,45,60,80,100' are entered" do
-      expect(report("1,10,45,60,80,100")).to eq("Green: 2\nAmber: 1\nRed: 3")
+      expect(report('1,10,45,60,80,100')).to eq("Green: 2\nAmber: 1\nRed: 3")
     end
   end
 end
